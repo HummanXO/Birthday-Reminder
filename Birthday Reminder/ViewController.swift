73b894b92ad965698735cc021e573236196a1778
HummanXO <rxkolchin@gmail.com>
2025-01-16 10:10:20 +0700

    Initial Commit

---
+++ Birthday Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birthday Reminder
+//
+//  Created by Alexandr on 1/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
