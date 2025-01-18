a6c34bddd75f58807f4c7a3b6571ad82572b9a86
HummanXO <rxkolchin@gmail.com>
2025-01-18 15:23:07 +0700

    finished

--- Birthday Reminder/View/AddBirthdayViewController.swift
+++ Birthday Reminder/View/AddBirthdayViewController.swift
@@ -1,9 +1,350 @@
 import UIKit
+import Foundation
 
-class AddBirthdayViewController: UIViewController {
-    override func viewDidLoad() {
+class AddBirthdayViewController: UIViewController, UITextFieldDelegate {
     
-        super.viewDidLoad()
-        view.backgroundColor = .red
+    private let namelabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter name"
+        return tf
+    }()
+    
+    private let nameSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Date of Birth"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        if #available(iOS 13.4, *) {
+            picker.preferredDatePickerStyle = .wheels
+        } else {
         }
+        picker.locale = Locale(identifier: "ru_RU")
+        return picker
+    }()
+    
+    private let dateTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter date"
+        return tf
+    }()
+    
+    private let dateSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let ageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Age"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let ageTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter age"
+        return tf
+    }()
+    
+    private let agePicker: UIPickerView = {
+        let picker = UIPickerView()
+        return picker
+    }()
+    
+    private let ageSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let genderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Gender"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let genderTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter gender"
+        return tf
+    }()
+    
+    private let genderPicker: UIPickerView = {
+        let picker = UIPickerView()
+        return picker
+    }()
+    
+    let genderOptions: [String] = ["Male", "Female", "Other"]
+    
+    private let genderSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let instagramLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Instagram"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let instagramTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter Instagram"
+        return tf
+    }()
+    
+    private lazy var instagramAlertController: UIAlertController = {
+        let alertController = UIAlertController(title: "Enter your Instagram", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter your Instagram"
+        }
+        
+        let doneAction = UIAlertAction(title: "Done", style: .default) { [weak self] _ in
+            if let text = alertController.textFields?.first?.text, !text.isEmpty {
+                self?.instagramTextField.text = text  // Устанавливаем текст в текстовое поле
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(doneAction)
+        
+        return alertController
+    }()
+    
+    private let instagramSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+        setupDatePicker()
+        setupAgePicker()
+        setupGenderPicker()
+        setupInstagramAlert()
+        hideKeyboardWhenTapptedAround()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: nil)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: nil)
+        }
+    
+    private func setupLayout() {
+        [namelabel, nameTextField, nameSeparatorView, dateLabel, dateTextField, dateSeparatorView, ageLabel, ageTextField, ageSeparatorView, genderLabel, genderTextField, genderSeparatorView, instagramLabel, instagramTextField, instagramSeparatorView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            namelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            namelabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            
+            nameTextField.topAnchor.constraint(equalTo: namelabel.bottomAnchor, constant: 10),
+            nameTextField.leadingAnchor.constraint(equalTo: namelabel.leadingAnchor),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            
+            nameSeparatorView.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 5),
+            nameSeparatorView.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            nameSeparatorView.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            nameSeparatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+            dateLabel.topAnchor.constraint(equalTo: nameSeparatorView.bottomAnchor, constant: 20),
+            dateLabel.leadingAnchor.constraint(equalTo: namelabel.leadingAnchor),
+            
+            dateTextField.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 10),
+            dateTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            dateTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            
+            dateSeparatorView.topAnchor.constraint(equalTo: dateTextField.bottomAnchor, constant: 5),
+            dateSeparatorView.leadingAnchor.constraint(equalTo: dateTextField.leadingAnchor),
+            dateSeparatorView.trailingAnchor.constraint(equalTo: dateTextField.trailingAnchor),
+            dateSeparatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+            ageLabel.topAnchor.constraint(equalTo: dateSeparatorView.bottomAnchor, constant: 20),
+            ageLabel.leadingAnchor.constraint(equalTo: namelabel.leadingAnchor),
+            
+            ageTextField.topAnchor.constraint(equalTo: ageLabel.bottomAnchor, constant: 10),
+            ageTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            ageTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            
+            ageSeparatorView.topAnchor.constraint(equalTo: ageTextField.bottomAnchor, constant: 5),
+            ageSeparatorView.leadingAnchor.constraint(equalTo: ageTextField.leadingAnchor),
+            ageSeparatorView.trailingAnchor.constraint(equalTo: ageTextField.trailingAnchor),
+            ageSeparatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+            genderLabel.topAnchor.constraint(equalTo: ageSeparatorView.bottomAnchor, constant: 20),
+            genderLabel.leadingAnchor.constraint(equalTo: namelabel.leadingAnchor),
+            
+            genderTextField.topAnchor.constraint(equalTo: genderLabel.bottomAnchor, constant: 10),
+            genderTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            genderTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            
+            genderSeparatorView.topAnchor.constraint(equalTo: genderTextField.bottomAnchor, constant: 5),
+            genderSeparatorView.leadingAnchor.constraint(equalTo: genderTextField.leadingAnchor),
+            genderSeparatorView.trailingAnchor.constraint(equalTo: genderTextField.trailingAnchor),
+            genderSeparatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+            instagramLabel.topAnchor.constraint(equalTo: genderSeparatorView.bottomAnchor, constant: 20),
+            instagramLabel.leadingAnchor.constraint(equalTo: namelabel.leadingAnchor),
+            
+            instagramTextField.topAnchor.constraint(equalTo: instagramLabel.bottomAnchor, constant: 10),
+            instagramTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            instagramTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            
+            instagramSeparatorView.topAnchor.constraint(equalTo: instagramTextField.bottomAnchor, constant: 5),
+            instagramSeparatorView.leadingAnchor.constraint(equalTo: instagramTextField.leadingAnchor),
+            instagramSeparatorView.trailingAnchor.constraint(equalTo: instagramTextField.trailingAnchor),
+            instagramSeparatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+        ])
+    }
+    
+    private func setupDatePicker() {
+        dateTextField.inputView = datePicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        dateTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc private func donePressed() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        formatter.locale = Locale(identifier: "ru_RU")
+        dateTextField.text = formatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+    
+    private func setupAgePicker() {
+        agePicker.dataSource = self
+        agePicker.delegate = self
+        ageTextField.inputView = agePicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ageDonePressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        ageTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc private func ageDonePressed() {
+        let selectedRow = agePicker.selectedRow(inComponent: 0)
+        ageTextField.text = "\(selectedRow + 1)"
+        view.endEditing(true)
+    }
+    
+    private func setupGenderPicker() {
+        genderPicker.dataSource = self
+        genderPicker.delegate = self
+        genderTextField.inputView = genderPicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(genderDonePressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        genderTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc private func genderDonePressed() {
+        genderTextField.text = genderOptions[genderPicker.selectedRow(inComponent: 0)]
+        view.endEditing(true)
+    }
+    
+    private func setupInstagramAlert() {
+        instagramTextField.delegate = self
+    }
+    
+    private func setupActions() {
+        
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == instagramTextField {
+            present(instagramAlertController, animated: true)
+            return false
+        }
+        return true
+    }
+    
+}
+
+extension AddBirthdayViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == agePicker {
+            return 100
+        } else if pickerView == genderPicker {
+            return genderOptions.count
+        }
+        return 0
+    }
+}
+
+extension AddBirthdayViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == agePicker{
+            return "\(row + 1)"
+        } else if pickerView == genderPicker {
+            return genderOptions[row]
+        }
+        return nil
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == agePicker {
+            ageTextField.text = "\(row + 1)"
+        } else if pickerView == genderPicker {
+            genderTextField.text = genderOptions[row]
+        }
+    }
+}
+
+extension AddBirthdayViewController {
+    func hideKeyboardWhenTapptedAround()  {
+        let gestureRecognizer = UITapGestureRecognizer()
+        gestureRecognizer.addTarget(self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
 }
--- Birthday Reminder/View/BirthdayListViewController.swift
+++ Birthday Reminder/View/BirthdayListViewController.swift
@@ -1,9 +1,101 @@
 import UIKit
 
 class BirthdayListViewController: UIViewController {
+    
+    private let nameLabel1: UILabel = {
+        let label = UILabel()
+        label.text = "Jeremy"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    private let dateLabel1: UILabel = {
+        let label = UILabel()
+        label.text = "18 дней"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let messageLabel1: UILabel = {
+        let label = UILabel()
+        label.text = "Happy Birthday, Jeremy!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let separatorView1: UIView = {
+        let separatorView = UIView()
+        separatorView.backgroundColor = .systemGray6
+        return separatorView
+    }()
+    
+    private let nameLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "John"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    private let dateLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "20 дней"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let messageLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "Happy Birthday, John!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let separatorView2: UIView = {
+        let separatorView = UIView()
+        separatorView.backgroundColor = .systemGray6
+        return separatorView
+    }()
+    
+    private let nameLabel3: UILabel = {
+        let label = UILabel()
+        label.text = "Anna"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    private let dateLabel3: UILabel = {
+        let label = UILabel()
+        label.text = "25 дней"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let messageLabel3: UILabel = {
+        let label = UILabel()
+        label.text = "Happy Birthday, Anna!"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let separatorView3: UIView = {
+        let separatorView = UIView()
+        separatorView.backgroundColor = .systemGray6
+        return separatorView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Birthday List"
+        setupLayout()
         view.backgroundColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
         }
@@ -12,5 +104,60 @@ class BirthdayListViewController: UIViewController {
         let addBirthdayVC = AddBirthdayViewController()
         let navController = UINavigationController(rootViewController: addBirthdayVC)
         present(navController, animated: true, completion: nil)
+    }
+    
+    private func setupLayout() {
+        [nameLabel1, nameLabel2, nameLabel3, dateLabel1, dateLabel2, dateLabel3, messageLabel1, messageLabel2, messageLabel3, separatorView1, separatorView2, separatorView3].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            
+            nameLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            nameLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            dateLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            dateLabel1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            messageLabel1.topAnchor.constraint(equalTo: nameLabel1.bottomAnchor, constant: 10),
+            messageLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            messageLabel1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            separatorView1.topAnchor.constraint(equalTo: messageLabel1.bottomAnchor, constant: 10),
+            separatorView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            separatorView1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            separatorView1.heightAnchor.constraint(equalToConstant: 2),
+            
+            nameLabel2.topAnchor.constraint(equalTo: separatorView1.bottomAnchor, constant: 20),
+            nameLabel2.leadingAnchor.constraint(equalTo: nameLabel1.leadingAnchor),
+            
+            dateLabel2.topAnchor.constraint(equalTo: nameLabel2.topAnchor),
+            dateLabel2.trailingAnchor.constraint(equalTo: dateLabel1.trailingAnchor),
+            
+            messageLabel2.topAnchor.constraint(equalTo: nameLabel2.bottomAnchor, constant: 10),
+            messageLabel2.leadingAnchor.constraint(equalTo: messageLabel1.leadingAnchor),
+            messageLabel2.trailingAnchor.constraint(equalTo: messageLabel1.trailingAnchor),
+            
+            separatorView2.topAnchor.constraint(equalTo: messageLabel2.bottomAnchor, constant: 10),
+            separatorView2.leadingAnchor.constraint(equalTo: separatorView1.leadingAnchor),
+            separatorView2.trailingAnchor.constraint(equalTo: separatorView1.trailingAnchor),
+            separatorView2.heightAnchor.constraint(equalToConstant: 2),
+            
+            nameLabel3.topAnchor.constraint(equalTo: separatorView2.bottomAnchor, constant: 20),
+            nameLabel3.leadingAnchor.constraint(equalTo: nameLabel1.leadingAnchor),
+            
+            dateLabel3.topAnchor.constraint(equalTo: nameLabel3.topAnchor),
+            dateLabel3.trailingAnchor.constraint(equalTo: dateLabel1.trailingAnchor),
+            
+            messageLabel3.topAnchor.constraint(equalTo: nameLabel3.bottomAnchor, constant: 10),
+            messageLabel3.leadingAnchor.constraint(equalTo: messageLabel1.leadingAnchor),
+            messageLabel3.trailingAnchor.constraint(equalTo: messageLabel1.trailingAnchor),
+            
+            separatorView3.topAnchor.constraint(equalTo: messageLabel3.bottomAnchor, constant: 10),
+            separatorView3.leadingAnchor.constraint(equalTo: separatorView1.leadingAnchor),
+            separatorView3.trailingAnchor.constraint(equalTo: separatorView1.trailingAnchor),
+            separatorView3.heightAnchor.constraint(equalToConstant: 2),
+        ])
     }
 }
